5188b46bbe4e2d293cc16bce158521add16b414d
BHASVIC-AlexWilkins24 <alex.wilkins24@students.bhasvic.ac.uk>
2024-11-17 17:10:14 +0000

    Finished check_in

--- Check_in.c
+++ Check_in.c
@@ -7,35 +7,85 @@
 #include <string.h>
 #include <time.h>
 
-#define MAX_GUESTS 4
+
+int room_rates[] = {100, 100, 85, 75, 75, 50};
+int board_rates[] = {20, 15, 5};
 
 
-#define ROOM1_2_PRICE 100
-#define ROOM3_PRICE 85
-#define ROOM4_5_PRICE 75
-#define ROOM6_PRICE 50
-
-// Board rates
-#define FULL_BOARD_RATE 20
-#define HALF_BOARD_RATE 15
-#define BB_RATE 5
-
-// Function to generate a random Booking ID
-void generateBookingID(char *surname, char *bookingID) {
-    int random_number = rand() % 1000;  // Random number between 0 and 999
-    sprintf(bookingID, "%s%d", surname, random_number);
+void generate_booking_id(char *surname, char *booking_id) {
+    srand(time(0));
+    int random_number = rand() % 10000;
+    sprintf(booking_id, "%s%d", surname, random_number);
 }
 
-// Function to get the room choice
-int getRoomChoice() {
-    int roomChoice;
-    do {
-        printf("\nChoose your room (1-6): ");
-        scanf("%d", &roomChoice);
-        if (roomChoice < 1 || roomChoice > 6) {
-            printf("Invalid room selection. Please select a room from 1 to 6.\n");
-        }
-    } while (roomChoice < 1 || roomChoice > 6);
-    return roomChoice;
-}
 
+int main() {
+    char user_first_name[50], user_surname[50], user_dob[20];
+    int total_guests, num_adults, num_children, board_type, length_of_stay;
+    int room_selection, newspaper_cost = 0;
+    char wants_newspaper[10], booking_id[60];
+
+
+    printf("Welcome to the Check-in System\n");
+
+
+    printf("Enter your first name: ");
+    scanf("%s", user_first_name);
+    printf("Enter your surname: ");
+    scanf("%s", user_surname);
+    printf("Enter your date of birth (DD/MM/YY): ");
+    scanf("%s", user_dob);
+
+
+    printf("Enter total number of guests: ");
+    scanf("%d", &total_guests);
+
+
+    printf("Enter the number of adults: ");
+    scanf("%d", &num_adults);
+
+
+    num_children = total_guests - num_adults;
+
+
+    printf("Select board type (1: Full Board, 2: Half Board, 3: Bed & Breakfast): ");
+    scanf("%d", &board_type);
+
+    printf("Enter the length of stay in days: ");
+    scanf("%d", &length_of_stay);
+
+
+    printf("Would you like a daily newspaper? (Yes/No): ");
+    scanf("%s", wants_newspaper);
+    if (strcmp(wants_newspaper, "Yes") == 0 || strcmp(wants_newspaper, "yes") == 0) {
+        newspaper_cost = 1;
+    }
+
+
+    printf("Available rooms and daily rates:\n");
+    for (int i = 0; i < 6; i++) {
+        printf("Room %d: $%d\n", i + 1, room_rates[i]);
+    }
+
+
+    printf("Select a room number (1-6): ");
+    scanf("%d", &room_selection);
+    int selected_room_cost = room_rates[room_selection - 1];
+
+
+    generate_booking_id(user_surname, booking_id);
+
+  
+    printf("\n--- Booking Details ---\n");
+    printf("Name: %s %s\n", user_first_name, user_surname);
+    printf("Date of Birth: %s\n", user_dob);
+    printf("Total Guests: %d (Adults: %d, Children: %d)\n", total_guests, num_adults, num_children);
+    printf("Board Type: %s\n", (board_type == 1) ? "Full Board" : (board_type == 2) ? "Half Board" : "Bed & Breakfast");
+    printf("Room Selected: Room %d ($%d per night)\n", room_selection, selected_room_cost);
+    printf("Length of Stay: %d days\n", length_of_stay);
+    printf("Daily Newspaper: %s ($%d per day)\n", (newspaper_cost == 1) ? "Yes" : "No", newspaper_cost);
+    printf("Booking ID: %s\n", booking_id);
+    printf("Thank you for checking in!\n");
+
+    return 0;
+}
